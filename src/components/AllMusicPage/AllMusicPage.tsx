f02fea60cbd8ed544b4711efae80498147fb3dbd
Supertiger <saawalkhn@googlemail.com>
2022-03-25 22:23:36 +0000

    fixed overflow using cheap solution :(

--- src/components/AllMusicPage/AllMusicPage.tsx
+++ src/components/AllMusicPage/AllMusicPage.tsx
@@ -12,29 +12,32 @@ export default function AllMusicPage() {
     setSongs(sorted);
   };
 
-  const rowRenderer = (prop: {key: string, index: number}) => (
+  const rowRenderer = (prop: {key: string, index: number, style: any}) => (
     <div key={prop.key} style={prop.style}>
-      <MusicTemplate music={songs[prop.index]} />
+      <MusicTemplate style={{ position: 'absolute', ...(!prop.index && { bottom: '5px' }) }} music={songs[prop.index]} />
     </div>
   );
   useEffect(() => {
     findAllMusic();
   }, []);
-  if (!songs.length) return <div>Loading...</div>;
+  if (!songs.length) return <div style={{ paddingTop: '100px' }}>Loading...</div>;
   return (
     <div className={styles.allMusicPage}>
-      <AutoSizer>
+      <AutoSizer className={styles.container}>
         {({ height, width }: AutoSizerProps) => (
           <List
             rowCount={songs.length}
-            rowHeight={45}
+            rowHeight={({ index }: any) => {
+              if (!index) return 100;
+              if (index >= songs.length - 1) return 125;
+              return 45;
+            }}
             width={width}
             height={height}
             rowRenderer={rowRenderer}
           />
         )}
       </AutoSizer>
-      {/* {songs.map((music) => <MusicTemplate key={music.path} music={music} />)} */}
     </div>
   );
 }
--- src/components/MusicTemplate/MusicTemplate.tsx
+++ src/components/MusicTemplate/MusicTemplate.tsx
@@ -4,11 +4,13 @@ import styles from './MusicTemplate.module.css';
 
 interface Props {
   music: Music
+  // eslint-disable-next-line react/require-default-props
+  style?: any
 }
 
 export default function MusicTemplate(props: Props) {
   return (
-    <div className={styles.music}>
+    <div className={styles.music} style={props.style}>
       <AlbumArt size={40} />
       <div className={styles.musicName}>{props.music.fileName}</div>
     </div>
